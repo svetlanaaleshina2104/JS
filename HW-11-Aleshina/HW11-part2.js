95644208a3adc694bc826aaa55577257a2375896
Svetlana Aleshina <Svetlana.aleshina2104@gmail.com>
2020-12-27 17:17:12 +0300

    changeHW11-1

---
+++ HW-11-Aleshina/HW11-part2.js
@@ -0,0 +1,9 @@
+let string1 = 'Нам необходимо';
+let string1 = 'сложить два ';
+const maxlength = 20;
+
+
+function concatTwoStrings(string1, string2) {
+    
+    return(string1, string2); 
+}
--- HW-11-Aleshina/HW11.js
+++ HW-11-Aleshina/HW11.js
@@ -1,14 +1,15 @@
 
-let word = 'bnfghyjrtshdytjkidyu';
-/* let myLenght = word.length */
 const maxlength = 10;
 
 function checkLength(word) {
-	console.log(word);
-}
+	
+	if (word.length < maxlength) {
+		console.log('Слово подходит!');
+	} else if (word.length >= maxlength) {
+		console.log('Слово слишком длинное!');
+	}
+	
+} 
 
-if (word.length < maxlength) {
-	checkLength ('Слово подходит!');
-} else if (word.length >= maxlength) {
-	checkLength('Слово слишком длинное!');
-}
+checkLength('Бронетранспортер');
+checkLength('Слово');
