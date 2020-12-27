8c1c02f1d28644eefb5800bb06a51b14fc26af4a
Svetlana Aleshina <Svetlana.aleshina2104@gmail.com>
2020-12-27 14:01:25 +0300

    FinishedPart1HW11

--- HW-11-Aleshina/HW11.js
+++ HW-11-Aleshina/HW11.js
@@ -1,29 +1,5 @@
-/* 
-function checkLength(word) {
-	// переданный аргумент word - переменная,
-	// которую и нужно проверять
 
-	// чтобы узнать длину строчки - используйте свойство .length
-}
-
-let word = 'илшдпдши';
-let empty = '';
-
-console.log('Слово илшдпдши занимает ' + word.length + ' кодовых значений');
-
-
-console.log('Пустая строка имеет длину, равную ' + empty.length);
-
-
-/* if (myNum > 3) {
-    myFunction('Больше трех!');
-} else if  (myNum < 3) {
-    myFunction('Меньше трех!');
-} else {
-    myFunction('Равен трем')
-} */ 
-
-let word = 'fgrtyrthybgtrhjkm';
+let word = 'bnfghyjrtshdytjkidyu';
 /* let myLenght = word.length */
 const maxlength = 10;
 
@@ -31,8 +7,8 @@ function checkLength(word) {
 	console.log(word);
 }
 
-if (word < maxlength) {
+if (word.length < maxlength) {
 	checkLength ('Слово подходит!');
-} else if (word > maxlength) {
+} else if (word.length >= maxlength) {
 	checkLength('Слово слишком длинное!');
 }
