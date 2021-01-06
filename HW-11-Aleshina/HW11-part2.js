9b6d9591d344bff6cf6bb6a612ac46d147e32f94
Svetlana Aleshina <Svetlana.aleshina2104@gmail.com>
2021-01-06 15:34:47 +0300

    AddSsecondPArt

--- HW-11-Aleshina/HW11-part2.js
+++ HW-11-Aleshina/HW11-part2.js
@@ -1,9 +1,24 @@
-let string1 = 'Нам необходимо';
-let string1 = 'сложить два ';
-const maxlength = 20;
+/* - Напишите функцию конкатенации (сложения) двух строк 
+(для знакомых с программированием - используйте интерполяционную строку), которая возвращает результат.
+
+Дополнительное условие со звездочкой - если результат больше чем 20 символов, 
+обрезать его, и добавлять троеточие в конце (используйте для получения подстроки метод [substring] */
+
+
+let string1 = 'Нам необходимо gerthuartjtghbrth';
+let string2 = 'сложить две строки';
+const maxLength = 20;
 
 
 function concatTwoStrings(string1, string2) {
+    let result = `${string1} ${string2}`;
+    if (result.length > maxLength) 
+        return `${result.substr(0, maxLength)}...`;
     
-    return(string1, string2); 
+ else 
+    return result;
+
 }
+
+console.log(concatTwoStrings(string1, string2))
+
